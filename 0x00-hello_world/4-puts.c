55ec2f0b09e3d19eb9f2c3faddc2d15091b819f1
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-06-10 13:29:12 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/*
+*main - puts
+*Return: 0
+*/
+int main(void)
+{
+printf("\"Programming is like building a multilingual puzzle, followed by a new line");
+return 0;
+}
