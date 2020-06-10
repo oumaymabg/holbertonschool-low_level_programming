3ed80f37b9ddaf01d70d7d579093fb6bf351aad3
oumaymabg <oumaymabou257@gmail.com>
2020-06-10 21:40:42 +0000

    trust me this will work

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,12 +4,12 @@ int intType;
 float floatType;
 char charType;
 long int longType;
-long long int longlongType
-printf("Size of a char: %d byte(s)\n", sizeof(charType));  
+long long int longlongType;
+printf("Size of a char: %c byte(s)\n", sizeof(charType));  
 printf("Size of an int:  %d byte(s)\n", sizeof(intType));
- printf("Size of a long int: %ld byte(s)\n", sizeof(longType)); 
-printf("Size of a long long int: %d byte(s)\n", sizeof(longlongType));
-printf("Size of a float: %d byte(s)\n", sizeof(floatType));
+ printf("Size of a long int: %li byte(s)\n", sizeof(longType)); 
+printf("Size of a long long int: %lli byte(s)\n", sizeof(longlongType));
+printf("Size of a float: %f byte(s)\n", sizeof(floatType));
 return(0);
 
 }
