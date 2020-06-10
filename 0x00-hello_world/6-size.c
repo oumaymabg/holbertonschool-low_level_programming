49f7e74bda6c314d278349d6dc69e5b934501eb9
oumaymabg <oumaymabou257@gmail.com>
2020-06-10 12:59:25 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
-int main()
+/**
+ *main - puts
+ *
+ *Return: 0
+ */
+int main(void)
 {
-  printf("\"with proper grammar, but the outcome is a piece of art,");
-
-return(0);
+  puts("\"with proper grammar, but the outcome is a piece of art,");
+  return 0;
 }
