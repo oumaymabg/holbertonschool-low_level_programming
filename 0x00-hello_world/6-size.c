910acc9989b18231d6bd3bc082f801156b97b008
oumaymabg <oumaymabou257@gmail.com>
2020-06-10 21:37:54 +0000

    try this one git

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
-/**
- *main - puts
- *
- *Return: 0
- */
-int main(void)
-{
-puts("\"with proper grammar, but the outcome is a piece of art,");
-return 0;
+#include<stdio.h>
+int main() {
+int intType;
+float floatType;
+char charType;
+long int longType;
+long long int longlongType
+printf("Size of a char: %d byte(s)\n", sizeof(charType));  
+printf("Size of an int:  %d byte(s)\n", sizeof(intType));
+ printf("Size of a long int: %ld byte(s)\n", sizeof(longType)); 
+printf("Size of a long long int: %d byte(s)\n", sizeof(longlongType));
+printf("Size of a float: %d byte(s)\n", sizeof(floatType));
+return(0);
+
 }
