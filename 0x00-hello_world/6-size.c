e2980d53e8e8973496b1120111803fc637f6b6ae
oumaymabg <oumaymabou257@gmail.com>
2020-06-10 22:12:01 +0000

    i did it nowgit add *

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@ float floatType;
 char charType;
 long int longType;
 long long int longlongType;
-printf("Size of a char: %lu byte(s)\n", sizeof(charType));  
-printf("Size of an int: %lu byte(s)\n", sizeof(intType));
-printf("Size of a long int: %lu byte(s)\n", sizeof(longType)); 
-printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongType));
-printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
+printf("Size of a char: %u byte(s)\n", sizeof(charType));  
+printf("Size of an int: %u byte(s)\n", sizeof(intType));
+printf("Size of a long int: %u byte(s)\n", sizeof(longType)); 
+printf("Size of a long long int: %u byte(s)\n", sizeof(longlongType));
+printf("Size of a float: %u byte(s)\n", sizeof(floatType));
 return (0);
 }
