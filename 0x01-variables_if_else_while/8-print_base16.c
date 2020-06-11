7f8c0061fe121cd702de0a6c274183d2d9f5f61d
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-06-11 22:43:04 +0100

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = '0'; ch <= '9'; ch++)
+putchar(ch);
+for (ch = 'a'; ch <= 'f'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
