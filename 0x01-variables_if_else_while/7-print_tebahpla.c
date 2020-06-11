b73dd99819ab9c1a53396004769b1100a9b230a7
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-06-11 23:00:41 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,10 +9,10 @@
 int main(void)
 {
 int ch;
-for (ch = '0'; ch <= '9'; ch++)
+for (ch = 'z'; ch <= 'a'; ch--)
+{
 putchar(ch);
-for (ch = 'a'; ch <= 'f'; ch++)
-putchar(ch);
+}
 putchar('\n');
 return (0);
 }
