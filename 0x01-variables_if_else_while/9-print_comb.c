0d64ece1a2d12b1faa4b0e0a2ffbf1b30b54b277
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-06-11 22:52:10 +0100

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n = '0'; n <= '9'; n++)
+{
+if (n != '0')
+{
+putchar(',');
+putchar(' ');
+}
+putchar(n);
+}
+putchar('\n');
+return (0)
