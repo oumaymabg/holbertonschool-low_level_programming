7bbdfad7635a0a345de370edeec669d35edbc688
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-06-11 22:44:27 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-char ch;
-for (ch = 'z'; ch >= 'a'; ch--)
+{
+int ch;
+for (ch = '0'; ch <= '9'; ch++)
+putchar(ch);
+for (ch = 'a'; ch <= 'f'; ch++)
 putchar(ch);
 putchar('\n');
 return (0);
