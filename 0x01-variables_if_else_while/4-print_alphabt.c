96dbe56bfd759f5082b8cae4e797cd49626c03ba
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-06-11 21:02:12 +0100

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' || ch != 'e' )
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
