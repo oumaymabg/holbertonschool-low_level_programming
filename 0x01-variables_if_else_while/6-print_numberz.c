772bd914763bc9178a46aae2f06a5b58284fd9e7
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-06-11 22:06:40 +0100

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int char;
+
+for (char = '0'; char <= '9'; char++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
