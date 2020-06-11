484a4b771bdb9a45294c70cbc009fa5772fa1364
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-06-11 20:00:08 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,14 +11,14 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
     {
-printf ("is positive");
+printf("is positive");
     }
 if (n == 0)
     {
-printf ("is zero");
+printf("is zero");
     }
 if (n < 0)
     {
-printf ("is negative");	
+printf("is negative");	
 return (0);
 }
