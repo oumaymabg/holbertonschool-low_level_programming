30cf22a715f62a11a0c1f37d157564802f74d0f6
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-06-11 11:20:26 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,21 +5,20 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-  int n;
+int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  if (n>0)
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n>0)
     {
-      printf ("is positive");
+printf ("is positive");
     }
-  if (n=0)
+if (n=0)
     {
-      printf ("is zero");
+printf ("is zero");
     }
-  if (n<0)
+if (n<0)
     {
-      printf ("is negative");
-	}
-  return (0);
+printf ("is negative");	
+return (0);
 }  
