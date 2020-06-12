48bea1bf33e884da8d7218173716dd932b5d1522
oumaymabg <oumaymabou257@gmail.com>
2020-06-12 11:50:11 +0000

    fix the commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,11 @@ char c;
 for (c = 'a'; c <= 'z'; c++)
 {
 putchar(c);
-	}
-	for (c = 'A'; c <= 'Z'; c++)
-	{
-		putchar(c);
-	}
-	putchar('\n');
-	return (0);
+        }
+        for (c = 'A'; c <= 'Z'; c++)
+        {
+                putchar(c);
+        }
+        putchar('\n');
+        return (0);
 }
