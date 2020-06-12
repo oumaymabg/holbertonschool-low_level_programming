d6b9d42e0d6e94445ef4b63c321b72f7a2904eb5
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-06-12 21:32:30 +0100

    Update 10-print_comb2.c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
-**/
-*
-*Return: Always 0 (Success)
-*/
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-        int i, j;
-
-	for (i = '0' ; i <= '9' ; i++)
-	{
-		for (j = '0' ; j <= '9' ; j++)
-		{
-			putchar(i);
-			putchar(j);
-			if (!((i == '9') && (j == '9')))
-			{
-				putchar(44);
-				putchar(32);
-			}
-		}
-	}
-	return (0);
+int i, j;
+for (i = '0' ; i <= '9' ; i++)
+{
+for (j = '0' ; j <= '9' ; j++)
+{
+putchar(i);
+putchar(j);
+if (!((i == '9') && (j == '9')))
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+return (0);
 }
