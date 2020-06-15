efa259c112e190aeaafbdc49971a97c15d3849de
oumaymabg <oumaymabou257@gmail.com>
2020-06-15 20:52:55 +0100

    functions nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+int i;
+    for (i = 'a'; i <= 'z'; i++)
+        _putchar(i);
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int _islower(int c)
+{
+    if (c >= 'a' && c <='z')
+        return (1);
+return(0);
+}
