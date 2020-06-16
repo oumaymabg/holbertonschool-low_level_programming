bf6eca1f204c5a0ff1bdba341527c13a2eba24d6
oumaymabg <oumaymabou257@gmail.com>
2020-06-16 18:15:11 +0000

    prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
 * main - Entry point
 *
@@ -8,6 +7,6 @@
 void print_alphabet(void)
 {
 int i;
-    for (i = 'a'; i <= 'z'; i++)
-        _putchar(i);
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
 }
