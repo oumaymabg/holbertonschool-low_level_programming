4551a2a73f6a53fa55f0a6669fafa94c48482c33
oumaymabg <oumaymabou257@gmail.com>
2020-06-16 20:11:19 +0000

    8th v1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,25 +1,22 @@
 #include "holberton.h"
-#include <stdlib.h>
-#include <time.h>
+#include "stdlib.h"
+#include "time.h"
 /**
-*void jack_bauer a function that prints every minute of the day of Jack Bauer,
+* jack_bauer - a function that prints every minute of the day of Jack Bauer,
 *
 * Return: starting from 00:00 to 23:59.
 */
 void jack_bauer(void)
 {
-int h ;
-int m ;
-for (h = 0; h <= 23; h++)
+int h;
+int m;
+for (h = '00'; h <= '23'; h++)
 {
-    for (m = 0; m <= 59; m++)
-    {
-        _putchar(h / 10 + '0');
-        _putchar(h % 10 + '0');
-        _putchar(':');
-        _putchar(m / 10 + '0');
-        _putchar(m % 10 + '0');
-        _putchar('\n');
-    }
-         }
-   }
+for (m = '00'; m <= '59'; m++)
+{
+_putchar(h);
+_putchar(':');
+_putchar(m);
+}
+}
+}
