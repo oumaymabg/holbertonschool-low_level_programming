ea5da4af7dccdcc43efc96ce36c09c19c41859d6
oumaymabg <oumaymabou257@gmail.com>
2020-06-16 11:52:57 +0100

    prints every minute of the day

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
+#include "math.h"
 /**
-* main - Entry point
-*
+*int print_last_digit
+*@n: print last digit
 * Return: Always 0 (Success)
 */
 int print_last_digit(int n)
 {
      if (n >= 10)
 {
- n = n / 10;
+ n = n % 10;
 }
-
 return (n);
 }
