00e94a9c9eca791cbe4af604387f9a114e8b67f8
oumaymabg <oumaymabou257@gmail.com>
2020-06-16 19:19:25 +0000

    second attempt

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- * _isalpha - a function that checks for lowercase character.
- * @c: char
- * Return: 1 or 0
- */
+* _isalpha - a function that checks for lowercase character.
+* @c: char
+* Return: 1 or 0
+*/
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
 }
