cc53381b63311b9b302a463399e218b189a5b347
oumaymabg <oumaymabou257@gmail.com>
2020-06-16 19:56:44 +0000

    the 7th v4

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 /**
+ * _abs - checking the sign of a variable
 * print_last_digit - check the code for Holberton School students.
 * @n: int
 * Return: 1 or 0 or -1
 */
-int sign(int n)
+int _abs(int n)
 {
 if (n >= 0)
 return (n);
@@ -12,7 +13,7 @@ return (-n);
 }
 int print_last_digit(int n)
 {
-n = sign(n);
+n = _abs(n);
 _putchar((n % 10) + '0');
 return (n % 10);
 }
