19c54b9cd5066f07e883088f6d181fe3fbefbea5
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-06-17 11:16:33 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,13 @@
 int print_last_digit(int n)
 {
 if (n < 0)
-n = (-1) * n;
-if (n >= 10)
 {
-return (n % 10);
+n = (-1) * n;
+  return (n % 10);
 }
-else
-return (n);
+else if (n >= 10)
+{
+  return (n % 10);
+
+}
 }
