b846edf21f8089fb89b4a53fb66b42f0aa519b4c
oumaymabg <oumaymabou257@gmail.com>
2020-06-17 22:00:06 +0100

    8th

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ for (h = 00; h <= 23; h++)
 {
 for (m = 00; m <= 59; m++)
 {
-_putchar(h / 10) + '0');
+_putchar((h / 10) + '0');
 _putchar((h % 10) + '0');
 _putchar(':');
 _putchar((m / 10) + '0');
