880c796f2ca558972d11eb5848c41991947f06cb
oumaymabg <oumaymabou257@gmail.com>
2020-06-17 21:20:36 +0000

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,9 @@
 void times_table(void)
 {
 int n, j, i;
-for (j=0; j <= 9; j++)
+for(j=0; j <= 9; j++)
 {
-for (i = 0; i <= 9; i++)
+for(i = 0; i <= 9; i++)
 {
 n = j * i;
 if(j !=0)
@@ -16,7 +16,7 @@ if(j !=0)
 _putchar(',');
 _putchar(',');
 }
-if (n > 9)
+if(n > 9)
 {
 _putchar(n / 10 + 48);
 _putchar(a % 10 + 48);
