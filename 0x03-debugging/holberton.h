a7986b9636bad77133fcbbc862faa9137cc1db0c
oumaymabg <oumaymabou257@gmail.com>
2020-06-18 21:08:46 +0000

    print remaining days

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,4 +5,6 @@
 
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void positive_or_negative(int i);
 #endif /* HOLBERTON_H */
