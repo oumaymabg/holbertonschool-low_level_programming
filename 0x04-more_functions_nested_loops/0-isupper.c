6f3e47e935b23e4bf08c5d2b99ec6b7dd6435522
oumaymabg <oumaymabou257@gmail.com>
2020-06-18 13:16:38 +0000

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* _isupper - a function that checks for uppercase charactery
+* @c: integer
+* Return: Always 0.
+*/
+int _isupper(int c)
+{
+while (c >= 'A' && c <= 'Z')
+return (1);
+return (0);
+}
