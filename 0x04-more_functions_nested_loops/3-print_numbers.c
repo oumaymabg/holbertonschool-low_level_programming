39428b7f89e722759dbb4a1e0d704060f50c4945
oumaymabg <oumaymabou257@gmail.com>
2020-06-18 17:35:12 +0000

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* print_numbers - print numbers
+*
+*
+*
+*/
+void print_numbers(void)
+{
+int a;
+a = 0;
+while (a <= 9)
+{
+_putchar(a + '0');
+a++;
+}
+_putchar('\n');
+}
