c18748ca91723a777f37dc3c8d091d396da0ccd7
oumaymabg <oumaymabou257@gmail.com>
2020-06-18 14:00:16 +0000

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - print digits
+ * @c: int
+ * Return: 0 or 1
+ *
+ */
+
+int _isdigit(int c)
+{
+while (c >= '0' && c <= '9')
+{
+return (1);
+}
+return (0);
+}
