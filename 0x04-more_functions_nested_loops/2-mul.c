f7fd9fb54e798d18fdcf2ec574a1e8b5252c5444
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-06-18 15:35:05 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,12 @@
 #include "holberton.h"
-
 /**
- * mul - multiplication table
- * @a: integer
- * @b: integer
- * Return: a * b
- */
+* mul - multiplication table
+* @a: integer
+* @b: integer
+* Return: a * b
+*/
 int mul(int a, int b)
 {
 mul = (a * b);
 return (mul);
 }
-
