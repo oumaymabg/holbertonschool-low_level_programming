d949ca9e3e519abf7be7abe066e3a8de87583e8a
oumaymabg <oumaymabou257@gmail.com>
2020-06-18 17:46:00 +0000

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* print_most_numbers - print digit except 2 and 4
+*
+*
+*
+*/
+void print_most_numbers(void)
+{
+int a;
+a = 0;
+while (a <= 9)
+{
+if (a != 2 && a != 4)
+_putchar(a % 10 + '0');
+a++;
+}
+_putchar('\n');
+}
