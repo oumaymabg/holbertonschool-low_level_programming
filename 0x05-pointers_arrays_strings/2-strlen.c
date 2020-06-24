050a95b847886f82886ce6e4d8bcc189022bf536
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-06-24 09:34:13 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 /**
-*strlen-2-strlen
+*_strlen-2-strlen
 *@s:pointer string
-*
+*@lng:int return value of _strlen
 */
 int _strlen(char *s)
 {
@@ -14,5 +14,5 @@ while (s[i] != '\0')
 lng++;
 i++;
 }
-return lng;
+return (lng);
 }
