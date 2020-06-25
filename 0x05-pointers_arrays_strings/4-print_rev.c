3659bdd31d9262feeb417c58f60aa9595119bf7b
oumaymabg <oumaymabou257@gmail.com>
2020-06-25 20:36:22 +0000

    puts reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* print_rev - puts reverse
+* @s:pointer
+*/
+void print_rev(char *s)
+{
+int i, j;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+for (j = i - 1; j >= 0; j--)
+{
+_putchar(s[j]);
+}
+_putchar('\n');
+}
+
+
