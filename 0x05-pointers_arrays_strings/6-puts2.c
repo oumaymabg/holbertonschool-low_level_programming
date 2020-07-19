b93240c331e208701b993fc5c21512707522aa61
oumaymabg <oumaymabou257@gmail.com>
2020-07-19 22:30:00 +0000

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 /**
-* puts2 - puts
-* @str: char pointer
-*/
+ * puts2 - puts
+ * @str: char pointer
+ */
 void puts2(char *str)
 {
-int j;
-for (j = 0; str[j] = '\0'; j = j + 2)
+int i;
+
+for (i = 0; str[i] != '\0'; i = i + 2)
 {
-_putchar(str[j]);
+_putchar(str[i]);
 }
 _putchar('\n');
 }
+
