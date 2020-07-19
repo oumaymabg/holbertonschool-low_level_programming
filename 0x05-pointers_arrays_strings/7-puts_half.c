d55c1aa4502af3dad03a6ae8e9db3b3b99ff878d
oumaymabg <oumaymabou257@gmail.com>
2020-07-19 22:31:44 +0000

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * puts_half - puts half string
+ * @str: char pointer
+ */
+void puts_half(char *str)
+{
+int i, j;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+}
+if (i % 2 == 0)
+for (j = i / 2; j < i; j++)
+{
+_putchar(str[j]);
+}
+else
+for (j = (i - 1) / 2; j < i; j++)
+{
+_putchar(str[j]);
+}
+_putchar('\n');
+}
+
