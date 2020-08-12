b3a58607fc21421170234f867c6a5625af53abe1
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-08-12 22:41:25 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 /**
- * puts_half - puts half string
- * @str: char pointer
+ * _strncat - strcat function
+ * @src: src pointer
+ * @dest: char pointer
  */
 char *_strcat(char *dest, char *src);
 
