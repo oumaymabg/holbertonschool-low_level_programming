7fa32dc3c033ffc2adc3add836c9c5a1a990725b
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-08-12 21:26:31 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 /**
-* print_array
-* @array: array pointer
+* print_array - display array
+* @a: array pointer
+* @n: integer
 */
 void print_array(int *a, int n);
 {
 int i;
 
-for (i = 0;i < n;i++)
+for(i = 0; i < n; i++)
 {
-printf (a[i], ","," ");
+printf(a[i], ",", " ");
 }
 _putchar('\n');
-return 0;
+return(0);
 }
