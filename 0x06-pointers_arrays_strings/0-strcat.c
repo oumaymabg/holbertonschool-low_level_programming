0411ef1aeb8eaae94c493302e32702a16a945cc2
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-08-12 22:39:11 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,24 +3,7 @@
  * puts_half - puts half string
  * @str: char pointer
  */
-void puts_half(char *str)
-{
-int i, j;
+char *_strcat(char *dest, char *src);
 
-for (i = 0; str[i] != '\0'; i++)
-{
-}
-if (i % 2 == 0)
-for (j = i / 2; j < i; j++)
-{
-_putchar(str[j]);
-}
-else
-for (j = (i - 1) / 2; j < i; j++)
-{
-_putchar(str[j]);
-}
-_putchar('\n');
-}
 
 
