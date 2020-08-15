b3f6b1847ada6570cb5a01395b959f4f3c2741f8
oumaymabg <oumaymabou257@gmail.com>
2020-08-15 11:53:14 +0000

    comments only

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* _strspn - a function that return the number of character in string
+* based on another string
+* @s: the initial string
+* @accept: the second string
+* Return: return the number of characters
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+}
