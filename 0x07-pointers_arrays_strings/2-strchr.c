0a61da9002439edd8623a6cf06460cf7df35c730
oumaymabg <oumaymabou257@gmail.com>
2020-08-15 11:35:19 +0000

    getting the character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* _strchr - a function that the pointer of a character
+* if it exists in a string and return null if it doesn't
+* @s: a string  that contain many characters
+* @c: the character that we need to look for
+* Return: the returned pointer on the character looked for
+*/
+char *_strchr(char *s, char c)
+{
+int found = 0;
+while (*s != '\0' || found == 1)
+{
+if (*s == c)
+return (s);
+found = 1;
+else
+s++;
+}
+return (NULL);
+}
