1192836e9418d736bdef415044e05b91054757e6
oumaymabg <oumaymabou257@gmail.com>
2020-08-17 11:19:41 +0000

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,8 @@
 #include "holberton.h"
+#include <stddef.h>
+#include <stdio.h>
 /**
-* _2-strchr -2-strchr a function that the pointer of a character
+* _strchr -strchr a function that the pointer of a character
 * if it exists in a string and return null if it doesn't
 * @s: a string  that contain many characters
 * @c: the character that we need to look for
