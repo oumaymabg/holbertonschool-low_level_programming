cd5e68ebbcbace5ce9eea733a7c6b896aba6dfe1
oumaymabg <oumaymabou257@gmail.com>
2020-08-18 16:19:19 +0000

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* _factorial -is the operation of multiplying any natural number with all the natural
+* based on another string
+* @n:return number
+* Return: return the number of characters
+*/
+int factorial(int n)
+{
+}
+
