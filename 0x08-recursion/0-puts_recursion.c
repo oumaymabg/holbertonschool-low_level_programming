9931032d928047343ead65e61b72acb1ab28ead7
oumaymabg <oumaymabou257@gmail.com>
2020-08-18 14:35:37 +0000

    puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+* _puts_recursion - a function that return the number of character in string
+* based on another string
+* @s:return string
+* Return: return the number of characters
+*/
+void _puts_recursion(char *s)
+{
+}
