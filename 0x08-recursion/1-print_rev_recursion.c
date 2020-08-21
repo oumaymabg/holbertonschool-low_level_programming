df42c56ff24814437b96562b2df0db082f9c511a
oumaymabg <oumaymabou257@gmail.com>
2020-08-21 21:24:21 +0000

    reversed

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,18 @@
 #include "holberton.h"
 /**
-* _puts_recursion - a function that return the number of character in string
-* based on another string
-* @s:return string
-* Return: return the number of characters
+* _print_rev_recursion - a function that the given string in reverse
+* @s: the giving string
+* Return: return the reversed string
 */
 void _print_rev_recursion(char *s)
 {
+if (*s == '\0')
+{
+_putchar('\n');
+}
+else
+{
+_print_rev_recursion(*s + 1);
+_putchar(*s);
+}
 }
