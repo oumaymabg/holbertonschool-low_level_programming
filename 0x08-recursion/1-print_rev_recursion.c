275672e8dd2be588798a80b87b28a2fbb840220e
oumaymabg <oumaymabou257@gmail.com>
2020-08-21 21:34:12 +0000

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,13 +6,13 @@
 */
 void _print_rev_recursion(char *s)
 {
-{
 if (*s == '\0')
 {
-putchar('\n');
+_putchar('\n');
 }
 else
 {
-_print_rev_recursion(*s + 1);
-putchar(*s);
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
 }
