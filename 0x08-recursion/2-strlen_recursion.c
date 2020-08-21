93bdb569d9d10c265e2c13e15b5ebf3a183bbee7
oumaymabg <oumaymabou257@gmail.com>
2020-08-21 21:50:51 +0000

    count

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,19 @@
 #include "holberton.h"
 /**
 * _strlen_recursion - a function that return the number of character in string
-* based on another string
-* @s:return string
+* @s:based string
 * Return: return the number of characters
 */
 int _strlen_recursion(char *s)
+char m = s;
 {
+if (*s == '\0')
+{
+_putchar('\n');
+return (s - m);
+}
+else
+{
+_strlen_recursion(s + 1);
+}
 }
