12c9de4f61d439fa5b8daeb505c3ad99dfb0c059
oumaymabg <oumaymabou257@gmail.com>
2020-10-12 12:31:38 +0000

    4-add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*main - 0
+*@argc: number_of_strings
+*@argv: number_of_arguments
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
