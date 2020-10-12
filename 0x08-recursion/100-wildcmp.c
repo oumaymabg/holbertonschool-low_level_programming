03da99b965560cee07dac7f5d6ab82213114c78e
oumaymabg <oumaymabou257@gmail.com>
2020-10-12 11:20:18 +0000

    100-will-cmp

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compare two strings
+ * string 2 may contain wildcards, which can expand to replace any string
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: 1 if strings match, 2 otherwise
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2 && *s1 != '\0')
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s1 == '\0')
+	{
+		if (*s2 == '\0')
+			return (1);
+
+		if (*s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+	}
+
+	if (*s2 == '*')
+		return ((wildcmp(s1 + 1, s2)) || (wildcmp(s1, s2 + 1)));
+
+	return (0);
+}
