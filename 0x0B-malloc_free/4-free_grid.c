cfdf8e3ac690c9b117103de6dbb4b7f256003b2d
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-10-12 21:32:37 +0100

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,17 +1,19 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
 /**
- * free_grid - frees a 2D grid previously created
- * @grid: input pointer to grid or 2D array
- * @height: height of 2D arrray
- * Return: void
+ * free_grid - clear a grid
+ * @grid: the grid to clear
+ * @height: height of the grid
+ *
  */
 void free_grid(int **grid, int height)
 {
 	int i;
 
-	for (i = 0; i < height; i++)
+	for (i = 0; i < height; ++i)
+	{
 		free(grid[i]);
+	}
 	free(grid);
 }
