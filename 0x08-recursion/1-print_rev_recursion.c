8704aa24f46a7ad80ae42810b0bdd2f201b5d345
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-10-12 12:16:06 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,13 +6,10 @@
 */
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
-{
-_putchar('\n');
-}
-else
-{
-_print_rev_recursion(s + 1);
-_putchar(*s);
-}
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
