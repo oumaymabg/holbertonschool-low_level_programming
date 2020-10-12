b1bce416b267b1712725812d0127c176b74fe066
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-10-12 11:59:39 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,4 +8,24 @@
 * Return: return the number of characters
 */
 char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				return (&s[i]);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (0);
+}
 
