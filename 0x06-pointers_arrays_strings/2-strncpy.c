6235360eb8f21af54ae9f08ef78903b5c9df4c1b
oumaymabg <oumaymabou257@gmail.com>
2020-10-12 10:30:55 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+*_strncpy - 0
+*@dest: string
+*@src: string
+*@n: int
+*Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; ++i)
+	{
+		dest[i] = src[i];
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
+
