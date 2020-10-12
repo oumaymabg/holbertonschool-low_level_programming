8c94fc820d2a1ec07551feb83c622f626d56cb25
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-10-12 11:58:33 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,4 +8,24 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
+  unsigned int i, j, n;
+
+	n = 0;
+	j = 0;
+	while (s[j] != '\0' && s[j] != ' ')
+	{
+		i = 0;
+		while (accept[i] != '\0')
+		{
+			if (accept[i] == s[j])
+			{
+				n += 1;
+			}
+			i++;
+		}
+		j++;
+	}
+
+
+	return (n);
 }
