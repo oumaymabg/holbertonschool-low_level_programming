eb7f921d2cb3587ea7c5eacb91fb5d2c93659ac5
oumaymabg <58654884+oumaymabg@users.noreply.github.com>
2020-10-12 11:56:12 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,13 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i = 0;
-while (i < n && src[i] != '\0')
-{
-dest[i] = src[i];
-i++;
+	unsigned int i;
+
+	for (i = 0; i < n; ++i)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
 }
-return (dest);
-}
+
